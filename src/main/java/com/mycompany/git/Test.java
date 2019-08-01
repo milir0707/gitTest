beac3ca6c81e500b02174a761ce7f613d09bbc00
lilee <lilee0707@163.com>
2019-08-01 21:02:22 +0800

    提交Test.java

---
+++ src/main/java/com/mycompany/git/Test.java
@@ -0,0 +1,21 @@
+/**
+ * @项目名：gitTest
+ * @创建人： lilee
+ * @创建时间： 2019-08-01
+ * @公司： www.mycompany.com
+ * @描述：TODO
+ */
+package com.mycompany.git;
+
+/**
+ * <p>NAME: Test</p>
+ * @author lilee
+ * @date 2019-08-01 20:59:28
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test2");
+    }
+}
