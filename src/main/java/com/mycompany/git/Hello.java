0e1128570c44a7261c45ba3b20dd531a667bcb8d
lilee <lilee0707@163.com>
2019-08-01 20:47:18 +0800

    提交整个项目

---
+++ src/main/java/com/mycompany/git/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * @项目名：gitTest
+ * @创建人： lilee
+ * @创建时间： 2019-08-01
+ * @公司： www.mycompany.com
+ * @描述：TODO
+ */
+package com.mycompany.git;
+
+/**
+ * <p>NAME: Hello</p>
+ * @author lilee
+ * @date 2019-08-01 18:45:41
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
