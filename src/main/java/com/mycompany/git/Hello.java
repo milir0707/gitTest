a1ab58edf757bd765dc72317c07a8b68f357a0ea
lilee <lilee0707@163.com>
2019-08-03 16:01:32 +0800

    合并测试：Hello.java

--- src/main/java/com/mycompany/git/Hello.java
+++ src/main/java/com/mycompany/git/Hello.java
@@ -17,5 +17,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!111222");
         System.out.println("Hello222");
+        System.out.println("merge");
     }
 }
