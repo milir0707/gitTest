51a8b6b015a7abd833e6ec445ecbad9abd835a70
lilee <lilee0707@163.com>
2019-08-03 15:34:32 +0800

    冲突测试：Hello.java

--- src/main/java/com/mycompany/git/Hello.java
+++ src/main/java/com/mycompany/git/Hello.java
@@ -15,7 +15,7 @@ package com.mycompany.git;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello!111");
         System.out.println("Hello222");
     }
 }
