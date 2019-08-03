f229281004388fd1fbde0b85e3fa71f1a4b8c343
lilee <lilee0707@163.com>
2019-08-03 15:37:11 +0800

    冲突测试：Hello.java_222

--- src/main/java/com/mycompany/git/Hello.java
+++ src/main/java/com/mycompany/git/Hello.java
@@ -15,7 +15,7 @@ package com.mycompany.git;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello!222");
         System.out.println("Hello222");
     }
 }
